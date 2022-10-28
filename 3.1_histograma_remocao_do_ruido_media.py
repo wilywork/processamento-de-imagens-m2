ee3698eb5f9457a7402191c273084c2fa7ffa483
WilyWork <xandy3_sjb@hotmail.com>
2022-10-28 12:59:50 -0300

    files;

--- 3.1_histograma_remocao_do_ruido_media.py
+++ 3.1_histograma_remocao_do_ruido_media.py
@@ -1,9 +1,9 @@
 import cv2
 from matplotlib import pyplot
 
-jetplaneImg = cv2.imread('./imagens_com_ruido_removido_media/jetplane.png', cv2.IMREAD_GRAYSCALE)
-lenaImg = cv2.imread('./imagens_com_ruido_removido_media/lena.png', cv2.IMREAD_GRAYSCALE)
-beeImg = cv2.imread('./imagens_com_ruido_removido_media/bee.png', cv2.IMREAD_GRAYSCALE)
+jetplaneImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_media/jetplane.png', cv2.IMREAD_GRAYSCALE)
+lenaImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_media/lena.png', cv2.IMREAD_GRAYSCALE)
+beeImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_media/bee.png', cv2.IMREAD_GRAYSCALE)
 
 pyplot.hist(jetplaneImg.ravel(), 256, [0,256])
 pyplot.show()
--- 3.3_histograma_remocao_do_ruido_mediana.py
+++ 3.3_histograma_remocao_do_ruido_mediana.py
@@ -1,9 +1,9 @@
 import cv2
 from matplotlib import pyplot
 
-jetplaneImg = cv2.imread('./imagens_com_ruido_removido_mediana/jetplane.png', cv2.IMREAD_GRAYSCALE)
-lenaImg = cv2.imread('./imagens_com_ruido_removido_mediana/lena.png', cv2.IMREAD_GRAYSCALE)
-beeImg = cv2.imread('./imagens_com_ruido_removido_mediana/bee.png', cv2.IMREAD_GRAYSCALE)
+jetplaneImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_mediana/jetplane.png', cv2.IMREAD_GRAYSCALE)
+lenaImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_mediana/lena.png', cv2.IMREAD_GRAYSCALE)
+beeImg = cv2.imread('./imagens_resultados/imagens_com_ruido_removido_mediana/bee.png', cv2.IMREAD_GRAYSCALE)
 
 pyplot.hist(jetplaneImg.ravel(), 256, [0,256])
 pyplot.show()
